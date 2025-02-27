cbe35832dc09aba3723c4fe51219d2e40e53feec
BsgMax00 <bram.engels22@gmail.com>
2025-02-27 18:10:20 +0100

    changed layout + added json server

--- src/Pages/Teambuilder.js
+++ src/Pages/Teambuilder.js
@@ -7,18 +7,33 @@ import { PokemonContext } from "../Context/PokemonContext";
 const TeamBuilder = () => {
     const { partyData } = useContext(PokemonContext);
 
+    const PartyDataEmpty = () => {
+        return partyData.every(item => item === "")
+    }
+
     return (
         <div>
             <div className="mx-4" style={{height: "90vh"}}>
-                <div className="d-flex align-items-center h-100">
-                    <div className="row  w-100">
-                        <center className="col-12 mb-3">
-                            <h2>Pick your pokemon for your team</h2>
-                        </center>
-                        {partyData.map((pokemon, index) => {
-                            return <PokemonCard key={index} pokemon={pokemon} index={index}/>
-                        })}
+                <div className="d-flex align-items-center h-75">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <center className="col-12 mb-3">
+                                <h2>Pick your pokemon for your team</h2>
+                            </center>
+                        </div>
+                        <div className="row">
+                            {partyData.map((pokemon, index) => {
+                                return <PokemonCard key={index} pokemon={pokemon} index={index}/>
+                            })}
+                        </div>
                     </div>
+                </div>
+                <div>
+                    {!PartyDataEmpty() && (
+                        <center>
+                            <button>Save Team</button>
+                        </center>
+                    )}
                 </div>
             </div>
                 
