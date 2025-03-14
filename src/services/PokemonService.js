604603bff05ee8b20d6d71498ae6a00ac2c2c6bd
BsgMax00 <bram.engels22@gmail.com>
2025-03-14 11:36:25 +0100

    forgot pokemon service, but has been fixed now.

--- src/services/PokemonService.js
+++ src/services/PokemonService.js
@@ -1,18 +1,12 @@
 import Pokemon from "../Classes/Pokemon"
+import { serviceFetch } from "./BaseService";
 
 export const serviceGetPokemonData = async() => {
-    try{
-        const response = await fetch("http://localhost:4000/pokemon");
-        if (!response.ok) {throw new Error("something went wrong in fetching all pokemon. (http://localhost:4000/pokemon)")};
-        const data = await response.json();
-
-        const convertedPokemonData = data.map(pokemon => 
-            new Pokemon(pokemon.id, pokemon.name, pokemon.imageUrl, pokemon.type)
-        );
-
-        return convertedPokemonData
-    }
-    catch(error) {
-        console.log(error)
-    };
+    const data = await serviceFetch("GET", "http://localhost:4000/pokemon")
+    
+    const convertedPokemonData = data.map(pokemon => 
+        new Pokemon(pokemon.id, pokemon.name, pokemon.imageUrl, pokemon.type)
+    );
+        
+    return convertedPokemonData;
 };
